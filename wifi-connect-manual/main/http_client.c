f03e244428d1563108d0a410101f793a78be7f82
Fabio <fs.embarcados@gmail.com>
2022-02-13 14:33:43 -0300

    Adicionando exemplo do wifi com conexão manual

---
+++ wifi-connect-manual/main/http_client.c
@@ -0,0 +1,60 @@
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_http_client.h"
+
+#include "http_client.h"
+
+static const char *TAG = "HTTP_CLIENT";
+
+
+//http client handle
+esp_err_t _http_event_handle(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER");
+            printf("%.*s", evt->data_len, (char*)evt->data);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                printf("%.*s", evt->data_len, (char*)evt->data);
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            break;
+    }
+    return ESP_OK;
+}
+
+//http request function
+void http_client_request()
+{
+    esp_http_client_config_t config = {
+    .url = "http://api.openweathermap.org/data/2.5/weather?q=Campinas,BR&APPID=2631b64b1f9374c56bd15a3c87c19574",
+    .event_handler = _http_event_handle,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+    ESP_LOGI(TAG, "Status = %d, content_length = %d",
+            esp_http_client_get_status_code(client),
+            esp_http_client_get_content_length(client));
+    }
+    esp_http_client_cleanup(client);
+}
---
+++ wifi-connect-manual/main/main.c
@@ -0,0 +1,76 @@
+/******************************************************************************
+* WIFI EXAMPLE - Connect Manual
+* This example shows how to connect to a WiFi network make a http request
+* This code is part of the course "Aprenda programar o ESP32 com ESP-IDF" by Fábio Souza
+* The course is available on https://cursos.embarcados.com.br
+*
+* This example code Creative Commons Attribution 4.0 International License.
+* When using the code, you must keep the above copyright notice,
+* this list of conditions and the following disclaimer in the source code.
+* (http://creativecommons.org/licenses/by/4.0/)
+
+* Author: Fábio Souza
+* This code is for teaching purposes only.
+* No warranty of any kind is provided.
+*******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+//freertos includes
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+
+//esp-idf includes
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+//commom component includes
+#include "protocol_examples_common.h"
+
+//http client includes
+#include "esp_http_client.h"
+
+#include "http_client.h"
+#include "wifi.h"
+
+//tag for logging
+static const char *TAG = "WIFI Example";
+
+xSemaphoreHandle wifi_semaphore;
+
+void http_client_task(void *pvParameters)
+{
+
+  xSemaphoreTake(wifi_semaphore, portMAX_DELAY);
+
+  while(1)
+  {
+    ESP_LOGI(TAG, "http_client_task");
+    http_client_request();                              //make http request
+    vTaskDelay(pdMS_TO_TICKS(10000));
+  }
+}
+
+//main func
+void app_main(void)
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_semaphore = xSemaphoreCreateBinary();
+    wifi_init_sta();    
+    xTaskCreate(&http_client_task, "http_client_task", 4096, NULL, 5, NULL);
+
+}
