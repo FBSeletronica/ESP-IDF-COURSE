7846b766c63ecfd677ebb2a3f66f4c83990712f1
Fabio <fs.embarcados@gmail.com>
2022-02-13 11:43:17 -0300

    Adicionando exemplo de wifi

---
+++ wifi-connect/main/main.c
@@ -0,0 +1,108 @@
+/******************************************************************************
+* WIFI EXAMPLE
+* This example shows how to connect to a WiFi network make a http request
+* This code is part of the course "Aprenda programar o ESP32 com ESP-IDF" by Fábio Souza
+* The course is available on https://cursos.embarcados.com.br
+*
+* This example code Creative Commons Attribution 4.0 International License.
+* When using the code, you must keep the above copyright notice,
+* this list of conditions and the following disclaimer in the source code.
+* (http://creativecommons.org/licenses/by/4.0/)
+
+* Author: Fábio Souza
+* This code is for teaching purposes only.
+* No warranty of any kind is provided.
+*******************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+//freertos includes
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+
+//esp-idf includes
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+//commom component includes
+#include "protocol_examples_common.h"
+
+//http client includes
+#include "esp_http_client.h"
+
+//tag for logging
+static const char *TAG = "WIFI Example";
+
+//http client handle
+esp_err_t _http_event_handle(esp_http_client_event_t *evt)
+{
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER");
+            printf("%.*s", evt->data_len, (char*)evt->data);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                printf("%.*s", evt->data_len, (char*)evt->data);
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            break;
+    }
+    return ESP_OK;
+}
+
+//http request function
+void http_client_request()
+{
+    esp_http_client_config_t config = {
+    .url = "http://api.openweathermap.org/data/2.5/weather?q=Campinas,BR&APPID=2631b64b1f9374c56bd15a3c87c19574",
+    .event_handler = _http_event_handle,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+    ESP_LOGI(TAG, "Status = %d, content_length = %d",
+            esp_http_client_get_status_code(client),
+            esp_http_client_get_content_length(client));
+    }
+    esp_http_client_cleanup(client);
+}
+
+
+//main func
+void app_main(void)
+{
+    //initialize NVS
+    ESP_ERROR_CHECK(nvs_flash_init());                  //initialize NVS
+    ESP_ERROR_CHECK(esp_netif_init());                  //initialize network interface
+    ESP_ERROR_CHECK(esp_event_loop_create_default());   //create default event loop
+
+    example_connect();                                  //connect to wifi
+    vTaskDelay(pdMS_TO_TICKS(5000));                    //wait 5 seconds
+    http_client_request();                              //make http request
+    example_disconnect();                               //disconnect from wifi
+
+}
