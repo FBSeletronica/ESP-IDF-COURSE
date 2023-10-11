54bad4766385ac2fd62409a34a3bd43e5278c079
Fábio Souza <fs.embarcados@gmail.com>
2023-10-11 15:26:17 -0300

    update DAC CW

--- DAC/dac-cosine-wave/main/dac_cosine_example_main.c
+++ DAC/dac-cosine-wave/main/dac_cosine_example_main.c
@@ -17,7 +17,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/dac_cosine.h"
-#include "esp_adc/adc_oneshot.h"
 #include "esp_log.h"
 
 static const char *TAG = "dac_cosine_example";
