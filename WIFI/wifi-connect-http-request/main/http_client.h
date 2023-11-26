b829c4b13bd5edac4010124f7d94d6305a0f39c5
Fábio Souza <fs.embarcados@gmail.com>
2023-11-26 15:23:15 -0300

    Update http_client.h

--- WIFI/wifi-connect-http-request/main/http_client.h
+++ WIFI/wifi-connect-http-request/main/http_client.h
@@ -2,6 +2,6 @@
 #define HTTP_CLIENT_H
 
 void http_client_request();  // Initialize the wifi station
-void http_client_task(void *pvParameters);   // Task to be run on the core 0
+void http_client_task(void *pvParameters)   // Task to be run on the core 0
 
 #endif // HTTP_CLIENT_H
