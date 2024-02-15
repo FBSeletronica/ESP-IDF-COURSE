27c7050d865e5570a38f612459d7f968dcfd9aa6
Fábio Souza <fs.embarcados@gmail.com>
2024-02-15 13:22:34 -0300

    update readme

--- extern-components/main/main.c
+++ extern-components/main/main.c
@@ -1,3 +1,19 @@
+/******************************************************************************
+* External components 
+* This example shows how to use External components libraries in ESP_IDF projects
+* This code is part of the course "Porgrame o ESP32 com ESP-IDF 5" by Fábio Souza
+* The course is available on https://cursos.embarcados.com.br
+*
+* This example code Creative Commons Attribution 4.0 International License.
+* When using the code, you must keep the above copyright notice,
+* this list of conditions and the following disclaimer in the source code.
+* (http://creativecommons.org/licenses/by/4.0/)
+
+* Author: Fábio Souza
+* This code is for teaching purposes only.
+* No warranty of any kind is provided.
+*******************************************************************************/
+
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
