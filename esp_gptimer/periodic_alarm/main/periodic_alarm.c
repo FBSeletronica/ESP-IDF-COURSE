fc19aed21753c226500d7138b27d5889572c8c63
FBSeletronica <fs.embarcados@gmail.com>
2025-07-13 11:57:52 -0300

    update example

--- esp_gptimer/periodic_alarm/main/periodic_alarm.c
+++ esp_gptimer/periodic_alarm/main/periodic_alarm.c
@@ -25,8 +25,8 @@
 #include "esp_log.h"
 
 #define BLED_GPIO (12)
-#define GLED_GPIO (BLED_GPIO + 1)
-#define RLED_GPIO (GLED_GPIO + 1)
+static const char *TAG = "main_app";
+static gptimer_handle_t gptimer = NULL;
 
 static esp_err_t s_init(void);
 static esp_err_t s_release(void);
@@ -34,9 +34,8 @@ static bool IRAM_ATTR s_timer_cb(gptimer_handle_t timer,
 					   const gptimer_alarm_event_data_t *edata,
 					   void *user_data);
 
-static const char *s_TAG = "main_app";
-static gptimer_handle_t gptimer = NULL;
 
+// Main application entry point
 void app_main(void)
 {
 	//Initilize timer and LED gpios
@@ -51,87 +50,111 @@ void app_main(void)
 
 esp_err_t s_init(void)
 {
-	esp_err_t rc;
+	esp_err_t ret;
 	
 	//Configuring LED GPIOs
 	const gpio_config_t gpio_handle = {
-		.pin_bit_mask = (1LLU << RLED_GPIO) | (1LLU << GLED_GPIO) | (1LLU << BLED_GPIO),
+		.pin_bit_mask = (1LLU << BLED_GPIO),
 		.mode = GPIO_MODE_OUTPUT,
 		.pull_up_en = GPIO_PULLUP_DISABLE,
 		.pull_down_en = GPIO_PULLDOWN_DISABLE,
 		.intr_type = GPIO_INTR_DISABLE
 	};
-	ESP_LOGI(s_TAG, "Configuring LED GPIOs");
-	if ((rc = gpio_config(&gpio_handle)))
-		return rc;
+	ESP_LOGI(TAG, "Configuring LED GPIOs");
+	if ((ret = gpio_config(&gpio_handle)))
+		return ret;
 
 	//Configuring gptimer
 	const gptimer_config_t timer_config = {
 		.clk_src = GPTIMER_CLK_SRC_DEFAULT,
 		.direction = GPTIMER_COUNT_UP,
-		.resolution_hz = 1000000 //1MHz, tick=1us
+		.resolution_hz = 1000000, 	//1MHz, tick=1us
 	};
-	ESP_LOGI(s_TAG, "Getting a new timer");
-	if ((rc = gptimer_new_timer(&timer_config, &gptimer)))
-		return rc;
+	ESP_LOGI(TAG, "Getting a new timer");
+	if ((ret = gptimer_new_timer(&timer_config, &gptimer)))
+		return ret;
 
-	ESP_LOGI(s_TAG, "Registering Callback");
+	ESP_LOGI(TAG, "Registering Callback");
 	const gptimer_event_callbacks_t timer_callbacks = {
 		.on_alarm = s_timer_cb
 	};
-	if ((rc = gptimer_register_event_callbacks(gptimer, &timer_callbacks, NULL)))
-		return rc;
+	if ((ret = gptimer_register_event_callbacks(gptimer, &timer_callbacks, NULL)))
+		return ret;
 
-	ESP_LOGI(s_TAG, "Enabling timer");
-	if ((rc = gptimer_enable(gptimer)))
-		return rc; //Timer has not started yet!
+	ESP_LOGI(TAG, "Enabling timer");
+	if ((ret = gptimer_enable(gptimer)))
+		return ret; //Timer has not started yet!
 	
-	ESP_LOGI(s_TAG, "Configuring the alarm");
+	ESP_LOGI(TAG, "Configuring the alarm");
 	gptimer_alarm_config_t alarm_config = {
 		.reload_count = 0,
 		.alarm_count = 100000, //eq to 0.1s
 		.flags.auto_reload_on_alarm = true //will reset the count on alarm
 	};
-	if ((rc = gptimer_set_alarm_action(gptimer, &alarm_config)))
-		return rc;
+	if ((ret = gptimer_set_alarm_action(gptimer, &alarm_config)))
+		return ret;
 	
-	ESP_LOGI(s_TAG, "Starting timer with period of %uus", (unsigned) alarm_config.alarm_count);
-	rc = gptimer_start(gptimer);
+	ESP_LOGI(TAG, "Starting timer with period of %uus", (unsigned) alarm_config.alarm_count);
+	ret = gptimer_start(gptimer);
 	
-	return rc;
+	return ret;
 }
 
 esp_err_t s_release(void)
 {
-	esp_err_t rc = ESP_OK;
+	    esp_err_t ret;
 
-	//Clearing gptimer
-	ESP_LOGI(s_TAG, "Clering gptimer");
-	rc += gptimer_stop(gptimer);
-	rc += gptimer_disable(gptimer);
-	rc += gptimer_del_timer(gptimer);
+	//Stop GPtimer
+    ESP_LOGI(TAG, "Stopping GPTimer");
+    ret = gptimer_stop(gptimer);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "gptimer_stop failed (%s)", esp_err_to_name(ret));
+        return ret;
+    }
 
-	//Reseting LED GPIOs
-	ESP_LOGI(s_TAG, "Reseting LED GPIOs");
-	rc += gpio_set_level(BLED_GPIO, 0);
-	rc += gpio_set_level(GLED_GPIO, 0);
-	rc += gpio_set_level(RLED_GPIO, 0);
+	//Disable GPTimer
+    ESP_LOGI(TAG, "Disabling GPTimer");
+    ret = gptimer_disable(gptimer);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "gptimer_disable failed (%s)", esp_err_to_name(ret));
+        return ret;
+    }
 
-	if (rc)
-		rc = ESP_FAIL;
+	//Delete GPTimer
+    ESP_LOGI(TAG, "Deleting GPTimer");
+    ret = gptimer_del_timer(gptimer);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "gptimer_del_timer failed (%s)", esp_err_to_name(ret));
+        return ret;
+    }
 
-	return rc;
+	// Resetting LED GPIO state
+    ESP_LOGI(TAG, "Resetting LED GPIO state");
+    ret = gpio_set_level(BLED_GPIO, 0);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "gpio_set_level failed (%s)", esp_err_to_name(ret));
+        return ret;
+    }
+
+    // Reset GPIO configuration to default state
+ 	ret = gpio_reset_pin(BLED_GPIO);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "gpio_reset_pin failed (%s)", esp_err_to_name(ret));
+        return ret;
+    } 
+
+    return ESP_OK;
 }
 
 bool s_timer_cb(gptimer_handle_t timer,
 				const gptimer_alarm_event_data_t *edata,
 				void *user_data)
 {
-	static DRAM_ATTR unsigned char counter = 0;
+	static DRAM_ATTR bool led_state = 0;
 
-	gpio_set_level(BLED_GPIO + counter++, 0);
-	counter = counter % 3;
-	gpio_set_level(BLED_GPIO + counter, 1);
+	//Toggle LED state
+	led_state ^= 1;
+	gpio_set_level(BLED_GPIO, led_state);
 	return false;
 }
 
